2b04d897a7640360d21c809d601c534d1875739f
zhangyu <zhangyu_1203@foxmail.com>
2018-11-28 10:45:53 +0800

    增加测试接口类

--- src/test/java/com/astar/sharetalent/SharetalentApplicationTests.java
+++ src/test/java/com/astar/sharetalent/SharetalentApplicationTests.java
@@ -1,16 +1,31 @@
 package com.astar.sharetalent;
 
+import com.astar.sharetalent.web.controller.UserWechatController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SharetalentApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
+
 	@Test
-	public void contextLoads() {
+	public void query() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/userwechat/query/1").accept(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 }
